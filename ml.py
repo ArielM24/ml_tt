fb96ad586d3d0bd48389da909f5b347c58f84083
ArielM24 <ariel_montoya15@outlook.es>
2021-10-31 17:52:50 -0600

    extract_model_forecast_data

--- ml.py
+++ ml.py
@@ -10,14 +10,48 @@ from pickle import dump, load
 ##  pip install numpy
 ##  pip install -U scikit-learn
 
-def extract_row_prices(json_row):
+def extract_row_prices(json_row, max = 10000):
     data = []
+    counter = 0
     for price in json_row["candles"]:
         data.append(price["open"])
         data.append(price["high"])
         data.append(price["low"])
         data.append(price["close"])
+        counter += 1
+        if counter == max:
+            break
     return np.array(data)
+
+def extract_forecast_prices(json_row, max = 10000):
+    data = []
+    counter = 0
+    for price in json_row:
+        candle = []
+        candle.append(price["open"])
+        candle.append(price["high"])
+        candle.append(price["low"])
+        candle.append(price["close"])
+        data.append(candle)
+        counter += 1    
+        if counter == max:
+            break
+    return np.array(data)
+
+def extract_model_forecast_data(js):
+    print("extracting data")
+    data = extract_forecast_prices(js)
+    pca_data = data.reshape(4, -1)
+    print(pca_data.shape)
+    pca = PCA(n_components=1)
+    pca.fit(pca_data)
+    pca_data = pca.transform(pca_data)
+    print(pca_data.shape)
+    components = []
+    for component in pca_data:
+        components.append(component[0])
+    return np.array(components).reshape(1, -1)
+
 
 def extract_data(json_data):
     data = []
@@ -30,9 +64,11 @@ def extract_model_data(js):
     data = extract_data(js)
     pca_x = data[:,:-1]
     y = data[:, -1].astype("str")
-    pca = PCA(n_components=10)
+    print(pca_x.shape)
+    pca = PCA(n_components=4)
     pca.fit(pca_x)
     pca_x = pca.transform(pca_x)
+    print(pca_x.shape)
     return train_test_split(pca_x, y, test_size=0.2)
 
 def get_model_metrics(model, x_test, y_test):
@@ -71,7 +107,8 @@ def make_prediction(model, x_data):
     return prediction
 
 if __name__ == "__main__":
-    file = open("eurusd_data/EURUSD1440.json")
+    #file = open("eurusd_data/EURUSD1440.json")
+    file = open("text.json")
     js = json.load(file)
     file.close()
 
@@ -81,7 +118,10 @@ if __name__ == "__main__":
 
     #Para cargar un modelo guardado (sin hacer otra vez train)
     model = load_model("model.bit")
-    x_train, x_test, y_train, y_test = extract_model_data(js)
+    #x_train, x_test, y_train, y_test = extract_model_data(js)
+    x_test= extract_model_forecast_data(js)
+    print(x_test.shape)
+
     #Generar prediccion (valores resultantes ichimoku)
     prediction = make_prediction(model, x_test)
     print(prediction, len(prediction))
